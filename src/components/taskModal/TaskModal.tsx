bcf9142f580bdba89378de34c269ed0571703fe2
Maikson <estevan.maik@gmail.com>
2022-09-23 16:06:51 -0300

    feat: implementa lógica que permite adicionar task à lista

--- src/components/taskModal/TaskModal.tsx
+++ src/components/taskModal/TaskModal.tsx
@@ -1,30 +1,81 @@
-import { useContext } from 'react';
+import { FormEvent, useContext } from 'react';
 import { TaskContext } from '../../context/TaskContext';
 import TaskModalCSS from './taskModal.module.css';
 
 function TaskModal() {
-  const { isModalOpen, setIsModalOpen } = useContext(TaskContext);
+  const { isModalOpen, setIsModalOpen, todoTasks, setTodoTasks } = useContext(TaskContext);
 
-  const handleCloseMOdal = () => {
+  const handleCloseModal = () => {
     setIsModalOpen(!isModalOpen);
-  }
+  };
+
+  function getTaskValue(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const { title: { value } } = e.target as typeof e.target & {
+      title: { value: string }
+    };
+    const { description } = e.target as typeof e.currentTarget;
+    createTask(value, description.value);
+  };
+
+  function createTask(title: string, description: string) {
+    const updatedTasks = todoTasks;
+    updatedTasks.push({
+      title,
+      description,
+      id: todoTasks.length + 1,
+    })
+    setTodoTasks([...updatedTasks]);
+    console.log(title, description);
+    console.log(todoTasks);
+  };
 
   return (
     <div
       className={ TaskModalCSS.modalContainer }
     >
-      <div
+      <section
         className={ TaskModalCSS.taskSettingsContainer }
       >
-        <div>
+        <div
+          className={ TaskModalCSS.taskSettingsHeader }
+        >
           <h4>Add a task</h4>
           <button
-            onClick={ handleCloseMOdal }
+            onClick={ handleCloseModal }
           >
             ×
           </button>
         </div>
-      </div>
+        <form
+          onSubmit={ (e) => getTaskValue(e) }
+          className={ TaskModalCSS.form }
+        >
+          <label htmlFor="title">
+            Title
+          </label>
+          <input
+            id="title"
+            className={ TaskModalCSS.titleInput }
+            name="title"
+            type="text"
+          />
+          <label htmlFor="description">
+            Description
+          </label>
+          <textarea
+            id="description"
+            className={ TaskModalCSS.descriptionInput }
+            name="description"
+          />
+          <button
+            className={ TaskModalCSS.createTaskBtn }
+            type="submit"
+          >
+            Create Task
+          </button>
+        </form>
+      </section>
     </div>
   )
 }
