d72bc28c3452fb8f8cbe0edea3ec5097570fb469
Maikson <estevan.maik@gmail.com>
2023-01-03 14:09:19 -0300

    refactor: removed repeated code

--- src/components/column/Column.tsx
+++ src/components/column/Column.tsx
@@ -13,122 +13,33 @@ function Column({ colName }: ColumnProps) {
 			<section className={ColumnCSS.bg}>
 				<p>{colName}</p>
 				<div className={ColumnCSS.taskContainer}>
-					{colName === 'To do' && (
-						<Droppable droppableId='todo'>
-							{(provided) => (
-								<div
-									{...provided.droppableProps}
-									ref={provided.innerRef}
-								>
-									{tasksState?.todo?.tasks?.map((task, i) => (
-										<Draggable
-											key={task.id}
-											draggableId={task.id}
-											index={i}
-										>
-											{(provided) => (
-												<div
-													{...provided.draggableProps}
-													{...provided.dragHandleProps}
-													ref={provided.innerRef}
-												>
-													<Task {...task} />
-												</div>
-											)}
-										</Draggable>
-									))}
-									{provided.placeholder}
-								</div>
-							)}
-						</Droppable>
-					)}
-					{colName === 'In Progress' && (
-						<Droppable droppableId='inProgress'>
-							{(provided) => (
-								<div
-									{...provided.droppableProps}
-									ref={provided.innerRef}
-								>
-									{tasksState?.inProgress?.tasks?.map((task, i) => (
-										<Draggable
-											key={task.id}
-											draggableId={task.id}
-											index={i}
-										>
-											{(provided) => (
-												<div
-													{...provided.draggableProps}
-													{...provided.dragHandleProps}
-													ref={provided.innerRef}
-												>
-													<Task {...task} />
-												</div>
-											)}
-										</Draggable>
-									))}
-									{provided.placeholder}
-								</div>
-							)}
-						</Droppable>
-					)}
-					{colName === 'In Review' && (
-						<Droppable droppableId='inReview'>
-							{(provided) => (
-								<div
-									{...provided.droppableProps}
-									ref={provided.innerRef}
-								>
-									{tasksState?.inReview?.tasks?.map((task, i) => (
-										<Draggable
-											key={task.id}
-											draggableId={task.id}
-											index={i}
-										>
-											{(provided) => (
-												<div
-													{...provided.draggableProps}
-													{...provided.dragHandleProps}
-													ref={provided.innerRef}
-												>
-													<Task {...task} />
-												</div>
-											)}
-										</Draggable>
-									))}
-									{provided.placeholder}
-								</div>
-							)}
-						</Droppable>
-					)}
-					{colName === 'Done' && (
-						<Droppable droppableId='done'>
-							{(provided) => (
-								<div
-									{...provided.droppableProps}
-									ref={provided.innerRef}
-								>
-									{tasksState?.done?.tasks?.map((task, i) => (
-										<Draggable
-											key={task.id}
-											draggableId={task.id}
-											index={i}
-										>
-											{(provided) => (
-												<div
-													{...provided.draggableProps}
-													{...provided.dragHandleProps}
-													ref={provided.innerRef}
-												>
-													<Task {...task} />
-												</div>
-											)}
-										</Draggable>
-									))}
-									{provided.placeholder}
-								</div>
-							)}
-						</Droppable>
-					)}
+					<Droppable droppableId='todo'>
+						{(provided) => (
+							<div
+								{...provided.droppableProps}
+								ref={provided.innerRef}
+							>
+								{tasksState?.todo?.tasks?.map((task, i) => (
+									<Draggable
+										key={task.id}
+										draggableId={task.id}
+										index={i}
+									>
+										{(provided) => (
+											<div
+												{...provided.draggableProps}
+												{...provided.dragHandleProps}
+												ref={provided.innerRef}
+											>
+												<Task {...task} />
+											</div>
+										)}
+									</Draggable>
+								))}
+								{provided.placeholder}
+							</div>
+						)}
+					</Droppable>
 				</div>
 			</section>
 		</>
