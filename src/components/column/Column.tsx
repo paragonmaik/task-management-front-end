5c135a992837ac826bd7279dddc475e3a13da800
Maikson <estevan.maik@gmail.com>
2023-01-03 16:58:51 -0300

    chore: moved addbtn to the column component

--- src/components/column/Column.tsx
+++ src/components/column/Column.tsx
@@ -1,46 +1,24 @@
 import { useContext } from 'react';
-import { Droppable, Draggable } from 'react-beautiful-dnd';
 import { ColumnProps } from '../../typescript/types';
 import { TaskContext } from '../../context/TaskContext';
 import Task from '../task/Task';
 import ColumnCSS from './column.module.css';
 
 function Column({ colName }: ColumnProps) {
-	const { tasksState } = useContext(TaskContext);
+	const { isModalOpen, setIsModalOpen } = useContext(TaskContext);
 
 	return (
 		<>
 			<section className={ColumnCSS.bg}>
 				<p>{colName}</p>
-				<div className={ColumnCSS.taskContainer}>
-					{/* <Droppable droppableId='todo'>
-						{(provided) => (
-							<div
-								{...provided.droppableProps}
-								ref={provided.innerRef}
-							>
-								{tasksState?.todo?.tasks?.map((task, i) => (
-									<Draggable
-										key={task.id}
-										draggableId={task.id}
-										index={i}
-									>
-										{(provided) => (
-											<div
-												{...provided.draggableProps}
-												{...provided.dragHandleProps}
-												ref={provided.innerRef}
-											>
-												<Task {...task} />
-											</div>
-										)}
-									</Draggable>
-								))}
-								{provided.placeholder}
-							</div>
-						)}
-					</Droppable> */}
-				</div>
+				<div className={ColumnCSS.taskContainer}></div>
+				<button
+					className={ColumnCSS.addBtn}
+					type='button'
+					onClick={() => setIsModalOpen(!isModalOpen)}
+				>
+					+
+				</button>
 			</section>
 		</>
 	);
