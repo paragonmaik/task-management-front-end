10f687d0d45f9138e832ad02af001e66d5e95e8a
Maikson <estevan.maik@gmail.com>
2023-01-05 15:58:24 -0300

    refactor: extracted modal container into a new component

--- src/components/taskModal/AddTaskModal.tsx
+++ src/components/taskModal/AddTaskModal.tsx
@@ -1,8 +1,8 @@
-import { useContext, useState, useRef } from 'react';
+import { useContext } from 'react';
 import { TaskContext } from '../../context/TaskContext';
-import { ISubTasks } from '../../typescript/types';
 import AddTaskModalCSS from './addTaskModal.module.css';
-import { createTask, closeAddTaskModal } from './AddTaskModalController';
+import { createTask } from './AddTaskModalController';
+import { Modal } from '../sub-components/modal/Modal';
 
 function AddTaskModal() {
 	const {
@@ -12,91 +12,37 @@ function AddTaskModal() {
 		setCreatedTasks,
 		createdTasks,
 	} = useContext(TaskContext);
-	const [subTasks, setSubTasks] = useState<ISubTasks[]>([]);
-	const subtaskInputRef = useRef<HTMLInputElement>(null);
-	const createSubTaskBtn = useRef<HTMLButtonElement>(null);
 
 	return (
-		<div
-			className={AddTaskModalCSS.modalContainer}
-			onClick={() => closeAddTaskModal(setIsModalOpen, isModalOpen)}
+		<Modal
+			modalHeader={`Add a task to the ${currentColumn.columnName} column`}
+			handleModal={setIsModalOpen}
+			isModalOpen
 		>
-			<section
-				className={AddTaskModalCSS.taskSettingsContainer}
-				// stops event bubbling so clicking anywhere besides the close modal
-				// button or the outer div won't close the modal
-				onClick={(e) => e.stopPropagation()}
-			>
-				<div className={AddTaskModalCSS.closeModalContainer}>
-					<h4>Add a task to {currentColumn.columnName} column</h4>
+			<div className={AddTaskModalCSS.containerScrollWrapper}>
+				<div className={AddTaskModalCSS.taskSettingsHeader}></div>
+				<form
+					onSubmit={(e) =>
+						createTask(e, createdTasks, setCreatedTasks, currentColumn._id)
+					}
+					className={AddTaskModalCSS.form}
+				>
+					<label htmlFor='description'>Description</label>
+					<textarea
+						id='description'
+						className={AddTaskModalCSS.descriptionInput}
+						name='description'
+						placeholder='Task description'
+					/>
 					<button
-						className={AddTaskModalCSS.closeModal}
-						onClick={() => closeAddTaskModal(setIsModalOpen, isModalOpen)}
+						className={AddTaskModalCSS.createTaskBtn}
+						type='submit'
 					>
-						×
+						Create Task
 					</button>
-				</div>
-				<div className={AddTaskModalCSS.containerScrollWrapper}>
-					<div className={AddTaskModalCSS.taskSettingsHeader}></div>
-
-					<form
-						onSubmit={(e) =>
-							createTask(e, createdTasks, setCreatedTasks, currentColumn._id)
-						}
-						className={AddTaskModalCSS.form}
-					>
-						<label htmlFor='description'>Description</label>
-						<textarea
-							id='description'
-							className={AddTaskModalCSS.descriptionInput}
-							name='description'
-							placeholder='Task description'
-						/>
-						<label htmlFor='sub-Tasks'>Subtasks</label>
-						{subTasks.length > 0 && (
-							<div className={AddTaskModalCSS.subTasksWrapper}>
-								{subTasks.map(({ description, id }) => (
-									<div
-										key={id}
-										className={AddTaskModalCSS.subTaskContainer}
-									>
-										<p>{description}</p>
-										<button
-											type='button'
-											// onClick={() => removeSubTaks(id)}
-											className={AddTaskModalCSS.deleteSubTaskBtn}
-										>
-											×
-										</button>
-									</div>
-								))}
-							</div>
-						)}
-						<input
-							ref={subtaskInputRef}
-							className={AddTaskModalCSS.subTaskInput}
-							type='text'
-							id='sub-Tasks'
-							placeholder='Subtask'
-						/>
-						<button
-							// onClick={createSubTask}
-							className={AddTaskModalCSS.createSubTaskBtn}
-							type='button'
-							ref={createSubTaskBtn}
-						>
-							Add Subtask
-						</button>
-						<button
-							className={AddTaskModalCSS.createTaskBtn}
-							type='submit'
-						>
-							Create Task
-						</button>
-					</form>
-				</div>
-			</section>
-		</div>
+				</form>
+			</div>
+		</Modal>
 	);
 }
 
