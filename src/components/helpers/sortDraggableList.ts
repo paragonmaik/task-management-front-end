0e359a5167987016fb63efc7f4296d21a32a6c3a
Maikson <estevan.maik@gmail.com>
2023-01-10 16:19:30 -0300

    feat: created task sorting function

--- src/components/helpers/sortDraggableList.ts
+++ src/components/helpers/sortDraggableList.ts
@@ -1,10 +1,11 @@
+import { ColumnState } from '../../typescript/types';
+
 export const sortDraggableList = (
-	listToBeSorted: any[] | null,
+	listToBeSorted: any[] | undefined,
 	sortedIdsList: any[] | undefined
 ) => {
 	if (!listToBeSorted) {
 		return 'List is missing';
-		// throw new Error('List is missing');
 	}
 	if (!sortedIdsList) {
 		return 'Ids list is missing';
@@ -13,4 +14,18 @@ export const sortDraggableList = (
 	listToBeSorted.sort(
 		(a, b) => sortedIdsList.indexOf(a._id) - sortedIdsList.indexOf(b._id)
 	);
+};
+
+export const sortDraggableTask = (
+	listToBeSorted: ColumnState[] | undefined
+) => {
+	if (!listToBeSorted) {
+		return 'List is missing';
+	}
+
+	for (const column of listToBeSorted) {
+		column.tasksList?.sort(
+			(a, b) => column.tasks?.indexOf(a._id) - column.tasks?.indexOf(b._id)
+		);
+	}
 };
