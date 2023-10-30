ed12c66ef8851a5c248f2f80418f6d939a767436
maik <estevan.maik@gmail.com>
2023-10-30 11:24:51 -0300

    feat: adds useBoard custom hook

---
+++ src/components/hooks/useBoard.tsx
@@ -0,0 +1,24 @@
+import { useQuery } from '@tanstack/react-query';
+import { axiosRequest } from '../../utils/axiosRequest';
+import { useLocalStorage } from './useLocalStorage';
+import { board } from '../../typescript/types';
+
+export default function useBoard() {
+  const [token, _setToken] = useLocalStorage('token', '');
+
+  return useQuery({
+    queryKey: ['programs'],
+    retry: 0,
+    queryFn: async (): Promise<board[]> => {
+      const res = await axiosRequest({
+        method: 'get',
+        url: '/board',
+        headers: {
+          Authorization: token,
+        },
+      });
+
+      return res.data;
+    },
+  });
+}
