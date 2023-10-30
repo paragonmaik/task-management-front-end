643bd9477df6d104f78823e9d53b8dada32c9699
maik <estevan.maik@gmail.com>
2023-10-30 14:03:30 -0300

    refactor: updates CreationForm component

--- src/components/sub-components/creationForm/CreationForm.tsx
+++ src/components/sub-components/creationForm/CreationForm.tsx
@@ -2,47 +2,20 @@ import CreationFormCSS from './creationForm.module.css';
 import { FormEvent } from 'react';
 import { InputField } from '../inputField/InputField';
 
-type CreationConfig = {
-	createdComponent: any;
-	setCreatedComponent: (createdComponent: any) => void;
-	token: string;
-	type: 'column' | 'board';
+type CreationFormProps = {
+  handleComponent: (e: FormEvent<HTMLFormElement>) => void;
+  type: 'column' | 'board';
 };
 
-interface CreationFormProps {
-	createComponent: (
-		e: FormEvent<HTMLFormElement>,
-		createdComponent: any,
-		setCreatedComponent: (createdComponent: any) => void,
-		token: string
-	) => void;
-	config: CreationConfig;
-}
-
-export const CreationForm = ({
-	createComponent,
-	config: { createdComponent, setCreatedComponent, token, type },
-}: CreationFormProps) => {
-	return (
-		<>
-			<form
-				onSubmit={(e) =>
-					createComponent(e, createdComponent, setCreatedComponent, token)
-				}
-				className={CreationFormCSS.formContainer}
-			>
-				<InputField
-					id={`${type}Name`}
-					placeholder={`add new a ${type}`}
-				/>
-
-				<button
-					type='submit'
-					className={CreationFormCSS.addBtn}
-				>
-					+
-				</button>
-			</form>
-		</>
-	);
+export const CreationForm = ({ handleComponent, type }: CreationFormProps) => {
+  return (
+    <form onSubmit={handleComponent} className={CreationFormCSS.formContainer}>
+      <div className="d-flex align-items-center w-100">
+        <InputField id={`${type}Name`} placeholder={`add new a ${type}`} />
+        <button type="submit" className={CreationFormCSS.addBtn}>
+          +
+        </button>
+      </div>
+    </form>
+  );
 };
