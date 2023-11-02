5cf40fca1b140a4595f9e0be69c426edd9cc91a1
maik <estevan.maik@gmail.com>
2023-11-02 19:01:33 -0300

    refactor: refactors Home component

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,14 @@ import {
   Route,
   Navigate,
 } from 'react-router-dom';
-import { useContext } from 'react';
-import { TaskContext } from './context/TaskContext';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import { ProtectedRoutes } from './ProtectedRoutes';
-import { lazy, Suspense } from 'react';
+import { Suspense } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Home = lazy(() => import('./components/home/Home'));
-
 function App() {
-  const { authUser } = useContext(TaskContext);
   return (
     <Router>
       <Routes>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import HomeCSS from '../components/home/home.module.css';
+import DashBoard from '../components/dashBoard/DashBoard';
+import SideBar from '../components/sideBar/SideBar';
+import useBoard from '../components/hooks/useBoard';
+import Header from '../components/header/Header';
+import { useContext, useState } from 'react';
+import { TaskContext } from '../context/TaskContext';
+
+export default function Home() {
+  const { data: boards } = useBoard();
+  const { currentBoardIdx } = useContext(TaskContext);
+  const [show, setShow] = useState(true);
+
+  return (
+    <div className={HomeCSS.container}>
+      <Header board={boards ? boards[currentBoardIdx] : undefined} />
+      <div className={HomeCSS.boardContainer}>
+        <SideBar boardsList={boards} show={show} setShow={setShow} />
+        <div className={show ? HomeCSS.visibleNav : HomeCSS.hiddenNav}>
+          <DashBoard board={boards ? boards[currentBoardIdx] : undefined} />
+        </div>
+      </div>
+    </div>
+  );
+}
