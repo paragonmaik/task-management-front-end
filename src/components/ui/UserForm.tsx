1af9a6a7814507ac4691ae3d7426c811dccaf2ff
maik <estevan.maik@gmail.com>
2023-11-03 09:21:08 -0300

    refactor: updates UserForm component

---
+++ src/components/ui/UserForm.tsx
@@ -0,0 +1,135 @@
+import { FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Alert from 'react-bootstrap/Alert';
+import UserFormCSS from './styles/userForm.module.css';
+import Loading from './Loading';
+
+type UserFormProps = {
+  children: JSX.Element | JSX.Element[];
+  isLoading: boolean;
+  navigateUrl: string;
+  formType: 'LOGIN' | 'REGISTER';
+  errorMessage: string | undefined;
+  handleSubmit: (e: FormEvent<HTMLFormElement>) => void;
+};
+
+export const UserForm = ({
+  children,
+  isLoading,
+  navigateUrl,
+  formType,
+  errorMessage,
+  handleSubmit,
+}: UserFormProps) => {
+  const nav = useNavigate();
+
+  return (
+    <div className={UserFormCSS.bg}>
+      <div className={UserFormCSS.formContainer}>
+        <h2>{formType === 'REGISTER' ? 'Create Account' : 'Sign in'}</h2>
+        <Form onSubmit={handleSubmit}>
+          {formType === 'REGISTER' ? (
+            <Form.Group className="mb-3" controlId="userName">
+              <FloatingLabel
+                controlId="userName"
+                label="Username"
+                className="mb-3"
+              >
+                <Form.Control
+                  style={{
+                    backgroundColor: 'rgb(45, 45, 57)',
+                    border: '2px solid hsl(240, 9%, 25%)',
+                    color: 'white',
+                  }}
+                  required
+                  type="text"
+                  placeholder="Username"
+                />
+              </FloatingLabel>
+            </Form.Group>
+          ) : null}
+          <Form.Group className="mb-3" controlId="email">
+            <FloatingLabel controlId="email" label="Email" className="mb-3">
+              <Form.Control
+                style={{
+                  backgroundColor: 'rgb(45, 45, 57)',
+                  border: '2px solid hsl(240, 9%, 25%)',
+                  color: 'white',
+                }}
+                required
+                type="email"
+                placeholder="Email"
+              />
+            </FloatingLabel>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="password1">
+            <FloatingLabel
+              controlId="password1"
+              label="Password"
+              className="mb-3"
+            >
+              <Form.Control
+                style={{
+                  backgroundColor: 'rgb(45, 45, 57)',
+                  border: '2px solid hsl(240, 9%, 25%)',
+                  color: 'white',
+                }}
+                required
+                type="password"
+                placeholder="Password"
+              />
+            </FloatingLabel>
+          </Form.Group>
+          {formType === 'REGISTER' ? (
+            <Form.Group className="mb-3" controlId="password2">
+              <FloatingLabel
+                controlId="password2"
+                label="Re-enter Password"
+                className="mb-3"
+              >
+                <Form.Control
+                  style={{
+                    backgroundColor: 'rgb(45, 45, 57)',
+                    border: '2px solid hsl(240, 9%, 25%)',
+                    color: 'white',
+                  }}
+                  required
+                  type="password"
+                  placeholder="Re-enter Password"
+                />
+              </FloatingLabel>
+            </Form.Group>
+          ) : null}
+          <br />
+          <div className="d-flex flex-column gap-2 justify-content-evenly">
+            <button className={UserFormCSS.loginBtn} type="submit">
+              {formType === 'REGISTER' ? 'Register' : 'Login'}
+            </button>
+            <button
+              onClick={() => nav(navigateUrl)}
+              className={UserFormCSS.createAccountBtn}
+              type="button"
+            >
+              {formType === 'REGISTER'
+                ? 'I already have an account'
+                : 'Register a new account'}
+            </button>
+          </div>
+          <div>
+            {errorMessage ? (
+              <Alert className="my-3" variant="danger">
+                {errorMessage}
+              </Alert>
+            ) : null}
+          </div>
+        </Form>
+        {children}
+        <div className="my-4 d-flex flex-column align-items-center">
+          {isLoading ? <Loading /> : null}
+        </div>
+      </div>
+    </div>
+  );
+};
