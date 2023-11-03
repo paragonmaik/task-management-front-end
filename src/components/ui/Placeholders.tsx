a435520e70a20a0d2b2d1cd2a498ff5d2f159aff
maik <estevan.maik@gmail.com>
2023-11-03 16:39:20 -0300

    feat: adds Placeholders component

---
+++ src/components/ui/Placeholders.tsx
@@ -0,0 +1,27 @@
+import Placeholder from 'react-bootstrap/Placeholder';
+
+export default function Placeholders() {
+  return (
+    <>
+      <Placeholder animation="glow">
+        <Placeholder xs={6} />
+      </Placeholder>
+
+      <Placeholder animation="glow">
+        <Placeholder xs={10} />
+      </Placeholder>
+
+      <Placeholder animation="glow">
+        <Placeholder xs={10} />
+      </Placeholder>
+
+      <Placeholder animation="glow">
+        <Placeholder xs={10} />
+      </Placeholder>
+
+      <Placeholder animation="glow">
+        <Placeholder xs={10} />
+      </Placeholder>
+    </>
+  );
+}
